8c5d62ac8963e7fe7f25b332c95e2dc3f6639ecd
Joel Barrantes Palacios <58517994+joelbarranteswins@users.noreply.github.com>
2024-05-10 03:22:49 -0500

    (Update) script.js updated

--- js/script.js
+++ js/script.js
@@ -8,3 +8,19 @@ document.addEventListener("DOMContentLoaded", function() {
         floatingWindow.style.display = "block";
     }, 2000); // 3000 milliseconds
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const openChatLinks = document.querySelectorAll(".openChat");
+    openChatLinks.forEach(function(link) {
+        link.addEventListener("click", function(event) {
+            event.preventDefault(); // Evitar que el enlace se comporte como un enlace normal
+            // Oculta el floating-window
+            const floatingWindow = document.getElementById("floating-window");
+            floatingWindow.style.display = "none";
+            // Abre el chat flotante
+            const chatContainer = document.getElementById("chatContainer");
+            chatContainer.style.display = "block";
+        });
+    });
+});
